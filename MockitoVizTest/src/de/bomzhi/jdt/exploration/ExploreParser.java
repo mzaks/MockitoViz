b03c7c5294d6ee96617aa65dc471bde1884ed7ea
Maxim Zaks <maxim.zaks@gmail.com>
2010-08-09 16:04:31 +0200

    dive deeper on parsing

--- MockitoVizTest/src/de/bomzhi/jdt/exploration/ExploreParser.java
+++ MockitoVizTest/src/de/bomzhi/jdt/exploration/ExploreParser.java
@@ -1,22 +1,49 @@
 package de.bomzhi.jdt.exploration;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static de.bomzhi.jdt.exploration.matcher.TypeHasName.hasTypeWithName;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.IsCollectionContaining.hasItem;
+
+import java.util.List;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExploreParser {
 	
-	@Test
-	public void parseSimpleClassDeclaration() throws Exception {
+	private ASTNode ast;
+
+	@Before
+	public void createAst() {
 		String javaCode = "class A {}";
 		ASTParser newParser = ASTParser.newParser(AST.JLS3);
 		newParser.setSource(javaCode.toCharArray());
-		ASTNode ast = newParser.createAST(new NullProgressMonitor());
+		ast = newParser.createAST(new NullProgressMonitor());
+	}
+	
+	@Test
+	public void parseSimpleClassDeclaration() throws Exception {
 		assertThat(ast, is(notNullValue()));
+	}
+	
+	@Test
+	public void enshureThatParsedElementIsAnCompilationUnit() throws Exception {
+		assertThat(ast, is(CompilationUnit.class));
+	}
+
+	@Test
+	public void enshureThatCompilationUnitHasTypeCalledA() throws Exception {
+		CompilationUnit cu = (CompilationUnit)ast;
+		List<AbstractTypeDeclaration> allTypes = cu.types();
+		assertThat(allTypes, is(notNullValue()));
+		assertThat(allTypes, hasItem(hasTypeWithName("A")));
 	}
 }
---
+++ MockitoVizTest/src/de/bomzhi/jdt/exploration/matcher/TypeHasName.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package de.bomzhi.jdt.exploration.matcher;
+
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+
+public class TypeHasName extends BaseMatcher<AbstractTypeDeclaration>{
+
+	private final String name;
+
+	public TypeHasName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public boolean matches(Object item) {
+		AbstractTypeDeclaration type = (AbstractTypeDeclaration)item;
+		return type.getName().getIdentifier().equals(name);
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("type with name ").appendValue(name);
+	}
+	
+	@Factory
+	public static TypeHasName hasTypeWithName(String typeName) {
+		return new TypeHasName(typeName);
+	}
+}
